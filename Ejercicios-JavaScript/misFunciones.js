6b13a0073a7d3979ee05a9e2748e3359ad8d30e0
mateoveda <2214715@ucc.edu.ar>
2023-05-16 09:54:26 -0300

    Se agregaron los cambios de la clase 16/05/23

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,30 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * Convierte unidades entre metros, pulgadas, pies y yardas;
+ * @method convertirUnidades
+ * @param {string} id - Id del elemento input html
+ * @param {number} valor - Es el valor que introduce el usuario para convertir
  */
+
+function convertirUnidades(id,valor){
+    if(isNaN(valor)){
+        alert("El valor ingresado es incorrecto")
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+    }else if(id=="metro"){
+        document.lasUnidades.unid_pulgada.value = valor*39.3701;
+        document.lasUnidades.unid_pie.value = valor*3.28084;
+        document.lasUnidades.unid_yarda.value = valor*1.09361;
+    }
+}
+
+function convertirGr(id){
+    if(id=="grados"){
+        let gr = document.getElementById("grados").value;
+        document.getElementById("radianes").value = gr*(Math.PI/180);
+    }else if(id=="radianes"){
+        let rad = document.getElementById("radianes").value;
+        document.getElementById("grados").value = rad*(180/Math.PI);
+    }
+}

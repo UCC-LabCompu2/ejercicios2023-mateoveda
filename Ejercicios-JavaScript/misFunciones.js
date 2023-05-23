0df7b6f218094189af1f0c2edb0d88e6bf77e319
mateoveda <2214715@ucc.edu.ar>
2023-05-23 09:01:33 -0300

    Se Agregaron parte de los ejercicios del 23/05/23

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -7,6 +7,11 @@
 
 convertirUnidades = (id,valor) => {
     let met, pul, pie, yar;
+
+    if(valor.includes(",")){
+        valor = valor.replace(",",".");
+    }
+
     if(isNaN(valor)){
         alert("El valor ingresado es incorrecto")
         met = "";
@@ -31,10 +36,10 @@ convertirUnidades = (id,valor) => {
         pie = valor * 3.28084;
         yar = valor * 1.09361;
     }
-    document.lasUnidades.unid_metro.value = met;
-    document.lasUnidades.unid_pulgada.value = pul;
-    document.lasUnidades.unid_pie.value = pie;
-    document.lasUnidades.unid_yarda.value = yar;
+    document.lasUnidades.unid_metro.value = Math.round(met*100)/100;
+    document.lasUnidades.unid_pulgada.value = Math.round(pul*100)/100;
+    document.lasUnidades.unid_pie.value = Math.round(pie*100)/100;
+    document.lasUnidades.unid_yarda.value = Math.round(yar*100)/100;
 }
 
 /**
@@ -81,4 +86,20 @@ let sumar = () => {
     s2 = Number(document.operacionesMat.sum_num2.value)
     res = s1 + s2;
     document.operacionesMat.sum_total.value = res;
+}
+
+let gererarUrl = () => {
+    const dist = document.getElementById("distancia").value;
+    const uni = document.getElementsByName("unidades")[0].value;
+    const urlCompl = `segundaWeb.html#${dist}#${uni}`;
+    window.open(urlCompl);
+}
+
+let cargarValor = () => {
+    let urlCompleta = window.location.href;
+    console.log(urlCompleta);
+    urlCompleta = urlCompleta.split('#');
+    const distancia = urlCompleta[1];
+    const unidad = urlCompleta[2];
+    doucment.getElementById("dist").value = `${distancia} ${unidad}`;
 }
